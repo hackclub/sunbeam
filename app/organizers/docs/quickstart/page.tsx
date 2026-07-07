7a3e8a1c4107c60a8c9cc11aa7d029b1934c6a97
yanellaft <yanellaft@gmail.com>
2026-07-07 13:10:56 -0400

    fixed quickstart guide on mobile

--- app/organizers/docs/quickstart/page.tsx
+++ app/organizers/docs/quickstart/page.tsx
@@ -5,21 +5,25 @@ import GuideContent from "../../../ultimateguide/GuideContent"
 export default function DocsQuickstart() {
   return (
     <div
-      className="flex min-h-screen lg:overflow-hidden"
+      className="flex h-screen overflow-hidden"
       style={{
         backgroundImage: "url('/imgs/sand.webp')",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div style={{position: "fixed", zIndex: 100}}>
-        <DocsSidebar />
-        <MobileNavbar />
-      </div>
+      <DocsSidebar />
 
-      <div className="lg:ml-[15vw]">
+      {/* desktop content */}
+      <div className="max-lg:hidden flex-1 overflow-y-auto">
         <GuideContent />
       </div>
+      {/* mobile content */}
+      <div className="lg:hidden flex-1 overflow-y-auto pb-48">
+        <GuideContent />
+      </div>
+
+      <MobileNavbar />
     </div>
   );
 }
