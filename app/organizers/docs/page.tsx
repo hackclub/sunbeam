1d16c7ef3cce70452971f426ee40ffcf331e7d06
yanellaft <yanellaft@gmail.com>
2026-07-08 14:06:25 -0400

    finished venue doc

--- app/organizers/docs/page.tsx
+++ app/organizers/docs/page.tsx
@@ -17,11 +17,22 @@ export default function OrganizerDocs() {
     >
       <DocsSidebar />
       {/* desktop content */}
-      <div className="max-lg:hidden">
-        <div className="flex flex-1 flex-col h-full"></div>
+      <div className="max-lg:hidden flex items-center justify-center h-screen text-center px-[5%]" style={{ fontSize: "20px", fontWeight: 400, color: "#2E599C", lineHeight: 1.6 }}>
+        <div className="flex flex-1 flex-col h-full">
+
+
+          <p style={{ fontSize: "2.33vw", fontWeight: 400, color: "#2E599C", lineHeight: 1.6, marginLeft: "3vw", paddingTop: "20%", textAlign: "center"}}>
+            Click on one of the resources on the <br/>right to view the documents. 
+          </p>
+
+
+        </div>
       </div>
+
       {/* mobile content */}
-      <div className="lg:hidden">hi</div>
+      <div className="lg:hidden flex items-center justify-center h-screen text-center px-[5%]" style={{ fontSize: "20px", fontWeight: 400, color: "#2E599C", lineHeight: 1.6 }}>
+        <p>Please view this tab on a computer to see every document.</p>
+      </div>
       <MobileNavbar />
     </div>
   );
