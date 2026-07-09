c253c52202898fbeae23af54060be6b5dcae3694
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-07-09 13:10:28 -0400

    Add in-progress warning on "your event" page

--- app/organizers/your-event/page.tsx
+++ app/organizers/your-event/page.tsx
@@ -1,7 +1,18 @@
-import Link from "next/link"
+import Image from "next/image";
 
 export default function YourEvent() {
-    return (
-        <></>
-    )
+	return (
+		<div className="min-h-screen bg-[#082249] flex flex-col items-center justify-center gap-8 text-center px-6">
+			<p className="outfit font-bold text-white text-4xl 2xl:text-6xl">
+				we&apos;re working on this! come back soon ;)
+			</p>
+			<Image
+				src="/imgs/sonbeam.png"
+				alt="Sonbeam"
+				width={1063}
+				height={1063}
+				className="w-[30vw] h-auto"
+			/>
+		</div>
+	);
 }
