c4079fbaba74a68f4cae5aa7ea3ce0802a50f2c7
lola sanchez <lola@hackclub.com>
2026-09-24 19:56:17 -0400

    did not mean to commit this

--- sunbeam-review/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: '/Users/lolasanchez/coding/hackcluball/sunbeam/sunbeam-review'
-  },
-};
-
-export default nextConfig;
